e961a1c8449690502d6fc08bb02aa03fccba0609
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-15 13:26:25 +0530

    ver 1.0

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="./logo.png" alt="log" width={100} />
+
+        <div className="h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
