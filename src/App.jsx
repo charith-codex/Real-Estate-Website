e81d4a7c4b8f0d408dfef6945e55414841398847
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-16 21:55:48 +0530

    ver 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./Header.css";
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
+      <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="log" width={100} />
 
-        <div className="h-menu">
+        <div className="flexCenter h-menu">
           <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
