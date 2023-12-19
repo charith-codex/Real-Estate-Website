9fd8f17c99f486c00c1a0f146f2670a599de895e
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-19 23:00:36 +0530

    ver 1.3

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./Hero.css"
+import {HiLocationMarker} from 'react-icons/hi'
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container ">
        {/* left side */}
-        <div className="hero-left"></div>
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle"/>
+            <h1>Discover <br /> Most Suitable <br /> Property</h1>
+          </div>
+          <div className="flexColStart hero-des">
+          <span>Find a variety of properties that suit you very easily</span>
+          <span>Forget all difficulties in finding a residence for you</span>
+        </div>
+        <div className="search-bar">
+          <HiLocationMarker color="var(--blue)" size={25}/>
+          <input type="text" />
+          <button className="button">Search</button>
+        </div>
+        </div>
+        
 
         {/* right side */}
         <div className="flexCenter hero-right">
