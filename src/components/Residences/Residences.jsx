a6b6112f98c05b6c8bd593c072b3b68e9fbd74ba
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-22 23:25:52 +0530

    ver 1.6

--- src/components/Residences/Residences.jsx
+++ src/components/Residences/Residences.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
-import "swiper/css"
-import './Residences.css'
-import data from '../../utils/slider.json'
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Residences.css";
+import data from "../../utils/slider.json";
 const Residences = () => {
   return (
     <section className="r-wrapper">
-        residences
-    </section>
-  )
-}
+      <section className="r-wrapper">
+        <div className="paddings innerWidth r-container">
+          <div className="r-head flexColStart">
+            <span className="orangeText">Best Choices</span>
+            <span className="primaryText">Popular Residences</span>
+          </div>
 
-export default Residences
+          <Swiper>
+            {data.map((card, i) => (
+                <SwiperSlide key={i}>
+                  <div className="flexColStart r-card">
+                    <img src={card.image} alt="home" />
+                    <span className="secondaryText r-price">
+                      <span style={{ color: "orange" }}>$</span>
+                      <span>{card.price}</span>
+                    </span>
+
+                    <span className="primaryText">{card.name}</span>
+                    <span className="secondaryText">{card.detail}</span>
+                  </div>
+                </SwiperSlide>
+              ))
+            }
+          </Swiper>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Residences;
