d06a7a2625f9752eda8d39e98321b22bb5120d9a
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-23 12:01:52 +0530

    ver 1.7

--- src/components/Residences/Residences.jsx
+++ src/components/Residences/Residences.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import "./Residences.css";
 import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
 const Residences = () => {
   return (
     <section className="r-wrapper">
@@ -13,22 +14,22 @@ const Residences = () => {
             <span className="primaryText">Popular Residences</span>
           </div>
 
-          <Swiper>
+          <Swiper {...sliderSettings}>
+            <SliderButtons />
             {data.map((card, i) => (
-                <SwiperSlide key={i}>
-                  <div className="flexColStart r-card">
-                    <img src={card.image} alt="home" />
-                    <span className="secondaryText r-price">
-                      <span style={{ color: "orange" }}>$</span>
-                      <span>{card.price}</span>
-                    </span>
+              <SwiperSlide key={i}>
+                <div className="flexColStart r-card">
+                  <img src={card.image} alt="home" />
+                  <span className="secondaryText r-price">
+                    <span style={{ color: "orange" }}>$</span>
+                    <span>{card.price}</span>
+                  </span>
 
-                    <span className="primaryText">{card.name}</span>
-                    <span className="secondaryText">{card.detail}</span>
-                  </div>
-                </SwiperSlide>
-              ))
-            }
+                  <span className="primaryText">{card.name}</span>
+                  <span className="secondaryText">{card.detail}</span>
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </section>
@@ -37,3 +38,13 @@ const Residences = () => {
 };
 
 export default Residences;
+
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="flexCenter r-buttons">
+      <button onClick={()=> swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=> swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
