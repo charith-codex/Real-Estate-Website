9d42a3a2a1f3f8641311863637b543c04ff608fa
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-12-25 21:35:47 +0530

    ver 1.9

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -10,7 +10,7 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import "./Value.css";
-
+import data from '../../utils/accordion'
 const Value = () => {
   return (
     <section className="v-wrapper">
@@ -30,6 +30,34 @@ const Value = () => {
             <br />
             We believe a good place to live can make your life better
           </span>
+
+          <Accordion
+          className="accordion"
+          allowMultipleExpanded={false}
+          preExpanded={[0]}
+          >
+            {data.map((item, i)=>{
+              return(
+                <AccordionItem className="accordionItem" key={i} uuid={i}>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span className="primaryText">
+                        {item.heading}
+                      </span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20}/>
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              )
+            })}
+          </Accordion>
+
         </div>
       </div>
     </section>
